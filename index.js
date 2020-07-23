72eba2eb647631eb168af352c8a3e1074dcee2ea
Adithya NR <adithyabhat17.ab@gmail.com>
2020-07-23 15:23:24 +0530

    :pencil: log error message before removing the node from DOM

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const flash = document.querySelector(".flash-error");
 
-if (flash.textContent.includes("problem billing your account")) flash.remove();
+if (flash.textContent.includes("problem billing your account")) {
+  console.log(flash.textContent);
+  flash.remove();
+}
 
 // Boom
